e2c2e63e88772fc91b92071d68d01135c842306f
ZeroToH3ro <mtblaser2002@gmail.com>
2023-07-28 12:48:40 +0700

    add from zero dev react-router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import './categories.styles.scss';
+import Home from "./routes/home/home.components";
+import Navigation from "./routes/navigation/navigation.components";
+import {Route, Routes} from "react-router-dom";
+import SignIn from "./routes/sign-in/sign-in.components";
+const Shop = () => {
+    return <h1>I am the shop pages</h1>
+}
 
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'Hats'
-        },
-        {
-            id: 2,
-            title: 'Jackets'
-        },
-        {
-            id: 3,
-            title: 'Sneakers'
-        },
-        {
-            id: 4,
-            title: 'Womens'
-        },
-        {
-            id: 1,
-            title: 'Mens'
-        },
-    ];
-
     return (
-        <div className='categories-container'>
-            {categories.map(({title}) => (
-                <div className='category-container'>
-                    <div className='background-image'/>
-                    <div className='category-body-container'>
-                        <h2>{title}</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+        <Routes>
+            <Route path="/" element={<Navigation/>}>
+                <Route index element={<Home/>}/>
+                <Route path='shop' element={<Shop/>}/>
+                <Route path='sign-in' element={<SignIn/>}/>
+            </Route>
+        </Routes>
+    )
 };
 
 export default App;
